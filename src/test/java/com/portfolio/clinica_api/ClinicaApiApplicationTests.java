b0ff1679e33f143cb033bd7d9a68f907fd70f5f1
valentino <vlmvalenlozanomassey@gmail.com>
2026-07-30 22:57:36 -0300

    primer commit

---
+++ src/test/java/com/portfolio/clinica_api/ClinicaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.clinica_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
